593989c2f60c28c817661277f4e246a6402291cb
valerio sgura <valeriosgura88@gmail.com>
2021-10-26 16:54:27 +0200

    GlobalStyle

--- prova_modal/src/App.js
+++ prova_modal/src/App.js
@@ -1,4 +1,5 @@
 import './index.css';
+import GlobalStyle from "./globalStyles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppEvaluationAdmin2 from "./components/committee/AppEvaluationAdmin2/AppEvaluationAdmin2";
 import AppAskAdmin from './components/committee/AppAskAdmin/AppAskAdmin';
@@ -14,21 +15,24 @@ import InProspectusAccept from './components/invest/InProspectusAccept/InProspec
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path='/' exact />
-        <Route path='/ae1' exact component={AppEvaluation1} />
-        <Route path='/ae2' exact component={AppEvaluation2} />
-        <Route path='/ae3' exact component={AppEvaluation3} />
-        <Route path='/aea1' exact component={AppEvaluationAdmin1} />
-        <Route path='/aea2' exact component={AppEvaluationAdmin2} />
-        <Route path='/login' exact component={Login} />
-        <Route path='/aaa' exact component={AppAskAdmin} />
-        <Route path='/indeclaration' exact component={InDeclaration} />
-        <Route path='/inprospectusaccept' exact component={InProspectusAccept} />
-      </Switch>
-    </Router>
+    <>
+    <GlobalStyle />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact />
+          <Route path='/ae1' exact component={AppEvaluation1} />
+          <Route path='/ae2' exact component={AppEvaluation2} />
+          <Route path='/ae3' exact component={AppEvaluation3} />
+          <Route path='/aea1' exact component={AppEvaluationAdmin1} />
+          <Route path='/aea2' exact component={AppEvaluationAdmin2} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/aaa' exact component={AppAskAdmin} />
+          <Route path='/indeclaration' exact component={InDeclaration} />
+          <Route path='/inprospectusaccept' exact component={InProspectusAccept} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
