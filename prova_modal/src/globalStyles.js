a5f0dca27253c3d4aaf016e4e5d68d22ca56d032
valerio sgura <valeriosgura88@gmail.com>
2021-10-26 18:28:59 +0200

    Import font, @font-face

--- prova_modal/src/globalStyles.js
+++ prova_modal/src/globalStyles.js
@@ -1,11 +1,79 @@
 import { createGlobalStyle } from "styled-components";
+import DMSansBold from './fonts/DMSans-Bold.ttf';
+import DMSansBoldItalic from './fonts/DMSans-BoldItalic.ttf';
+import DMSansItalic from './fonts/DMSans-Italic.ttf';
+import DMSansMediumItalic from './fonts/DMSans-MediumItalic.ttf';
+import DMSansMedium from './fonts/DMSans-Medium.ttf';
+import DMSansRegular from './fonts/DMSans-Regular.ttf';
+
 
 const GlobalStyle = createGlobalStyle`
-  body {
+
+  @font-face {
+    font-family: 'DM Sans';
+    src: url(${DMSansItalic}) format('truetype');
+    font-weight: normal;
+    font-style: italic;
+    font-display: swap;
+  }
+
+  @font-face {
+      font-family: 'DM Sans';
+      src: url(${DMSansBoldItalic}) format('truetype');
+      font-weight: bold;
+      font-style: italic;
+      font-display: swap;
+  }
+
+  @font-face {
+      font-family: 'DM Sans';
+      src: url(${DMSansBold}) format('truetype');
+      font-weight: bold;
+      font-style: normal;
+      font-display: swap;
+  }
+
+  @font-face {
+      font-family: 'DM Sans';
+      src: url(${DMSansMediumItalic}) format('truetype');
+      font-weight: 500;
+      font-style: italic;
+      font-display: swap;
+  }
+
+  @font-face {
+      font-family: 'DM Sans';
+      src: url(${DMSansMedium}) format('truetype');
+      font-weight: 500;
+      font-style: normal;
+      font-display: swap;
+  }
+
+  @font-face {
+      font-family: 'DM Sans';
+      src: url(${DMSansRegular}) format('truetype');
+      font-weight: normal;
+      font-style: normal;
+      font-display: swap;
+  }
+
+  /* @font-face {
+      font-family: 'DM Sans';
+      src: url('') format('truetype');
+      font-weight: normal;
+      font-style: normal;
+      font-display: swap;
+  } */
+
+  * {
+    font-family: 'DM Sans';
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
     background: grey;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
   }
 `;
 
